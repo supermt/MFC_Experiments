1e7849fbddd2984bad8ef13a1512a3c22256d391
SuperMT <supertempler@gmail.com>
2020-05-26 16:13:10 +0800

    Fix the second tag

--- MFC_Experiments/QueueingDlg.cpp
+++ MFC_Experiments/QueueingDlg.cpp
@@ -24,6 +24,27 @@ QueueingDlg::~QueueingDlg()
 void QueueingDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialogEx::DoDataExchange(pDX);
+	
+	DDX_Control(pDX, IDC_LIST5, QueueingResultList);
+	DWORD dwstyle = QueueingResultList.GetExtendedStyle();
+	dwstyle |=LVS_EX_FULLROWSELECT;
+	dwstyle |= LVS_EX_GRIDLINES;
+	QueueingResultList.SetExtendedStyle(dwstyle);
+
+	int column_width = 60;
+	// initial the data in result list
+	QueueingResultList.InsertColumn(0,_T("Simulation"),LVCFMT_LEFT,column_width);
+	QueueingResultList.InsertColumn(1,_T("Average"),LVCFMT_LEFT,column_width);
+	QueueingResultList.InsertColumn(2,_T("Average"),LVCFMT_LEFT,column_width);
+	QueueingResultList.InsertColumn(3,_T("The time"),LVCFMT_LEFT,column_width);
+	QueueingResultList.InsertColumn(4,_T("The Aver"),LVCFMT_LEFT,column_width);
+	QueueingResultList.InsertColumn(5,_T("The ma"),LVCFMT_LEFT,column_width);
+	QueueingResultList.InsertColumn(6,_T("The ma"),LVCFMT_LEFT,column_width);
+	QueueingResultList.InsertColumn(7,_T("The ma"),LVCFMT_LEFT,column_width);
+	QueueingResultList.InsertColumn(8,_T("The ma"),LVCFMT_LEFT,column_width);
+	QueueingResultList.InsertColumn(9,_T("The ma"),LVCFMT_LEFT,column_width);
+	QueueingResultList.InsertColumn(10,_T("Balk"),LVCFMT_LEFT,column_width);
+
 }
 
 
@@ -32,3 +53,9 @@ END_MESSAGE_MAP()
 
 
 // QueueingDlg message handlers
+
+
+void QueueingDlg::OnBnClickedCheck1()
+{
+	// TODO: Add your control notification handler code here
+}
--- MFC_Experiments/QueueingDlg.h
+++ MFC_Experiments/QueueingDlg.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "afxcmn.h"
 
 
 // QueueingDlg dialog
@@ -18,4 +19,7 @@ protected:
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
 
 	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedCheck1();
+	CListCtrl QueueingResultList;
 };
